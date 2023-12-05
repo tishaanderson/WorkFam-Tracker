9c455f7d70473530e7a0aa1ff6f7c920f110ed44
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-12-04 23:28:09 -0600

    UPDATE SWITCH STATEMENT
    - Switch statment should be finalized. NEED TO create the functions for each case, though.
    - TODO::  Pull in functions once created.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
+//TODO: add a require const pulling in the functions i create for the switch statement
 
-//initial function to gather user input
+//initial function for user to choose their task
 function init() {
   inquirer.prompt([
     {
@@ -19,6 +20,31 @@ function init() {
     }
   ]) //Switch statement to call the next function based on user input
   .then(answer => {
-    switch
-  })
+    switch (answer.task) {
+      case 'View all departments':
+        viewDepartments(); //TODO create a function to view all departments
+        break;
+      case 'View all roles':
+        viewRoles(); //TODO create a function to view all roles
+        break;
+      case 'View all employees':
+        viewEmployees(); //TODO create a function to view all employees
+        break;
+      case 'Add a role':
+        addNewRole(); //TODO create a function to add a role
+        break;
+      case 'Add an employee':
+        addNewEmployee(); //TODO create a function to add an employee
+        break;
+      case 'Update an employee role':
+        updateEmployee(); //TODO create a function to  update an employee
+        break;
+      default:
+        console.log('Please select a valid task.')
+        init();
+        break;   
+    }
+  });
 }
+
+init();
