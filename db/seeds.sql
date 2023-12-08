eaedc95d7e1f2cf07a1616cb2630f593eab1a8ce
Tisha Anderson <136303804+tishaanderson@users.noreply.github.com>
2023-12-07 23:59:42 -0600

    FIX BUG IN SQL FILES
    - The schema.sql file had the id's set to AUTOINCREMENT but the seeds.sql had id's manually entered. I removed the manually entered id's so mysql would assign those itself.
    
    - The id title was also removed from the seeds.sql values list.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
-INSERT INTO department (id, name)
-VALUES  (1, "Field Agents"), 
-        (2, "Administration"),
-        (3, "Support");
+INSERT INTO department (name)
+VALUES  ("Field Agents"), 
+        ("Administration"),
+        ("Support");
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES  (1, "Secret Agent", 100000, 1), 
-        (2, "Director", 150000, 2),
-        (3, "Comptroller", 90000, 2), 
-        (4, "HR Director", 85000, 2);
-        (5, "Technical Specialist", 95000, 3), 
-        (6, "Secretary", 60000, 2),
-        (7, "Head of Research", 110000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Secret Agent", 100000, 1), 
+        ("Director", 150000, 2),
+        ("Comptroller", 90000, 2), 
+        ("HR Director", 85000, 2),
+        ("Technical Specialist", 95000, 3), 
+        ("Secretary", 60000, 2),
+        ("Head of Research", 110000, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (1, "Sterling", "Archer", 1, NULL), 
-        (2, "Malory", "Archer", 2, NULL),
-        (3, "Lana", "Kane", 1, 2), 
-        (4, "Cyril", "Figgis", 4, 2), 
-        (5, "Cheryl", "Tunt", 6, 2),
-        (6, "Pam", "Poovey", 6, 2), 
-        (7, "Ray", "Gillette", 1, 2),
-        (8, "Algernop", "Krieger", 7, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Malory", "Archer", 2, 1),
+        ("Sterling", "Archer", 1, 1),
+        ("Lana", "Kane", 1, 2), 
+        ("Cyril", "Figgis", 4, 2), 
+        ("Cheryl", "Tunt", 6, 2),
+        ("Pam", "Poovey", 6, 2), 
+        ("Ray", "Gillette", 1, 2),
+        ("Algernop", "Krieger", 7, 1);
